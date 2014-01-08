c24e073e1898a41903651f73465a74e6377604eb
Annie Tang <yuetang@mit.edu>
2014-01-08 17:08:53 -0500

    heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Barfly::Application.config.secret_key_base = '12f1a9f64facd1255399dea1c020ad4110e0d289d7205913a5bb31b56e7b33bd9ff5409d9af74ce3e0d8eaf250e31521563e9c54a82956340f802a3a199d8a77'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Barfly::Application.config.session_store :cookie_store, key: '_barfly_session'
