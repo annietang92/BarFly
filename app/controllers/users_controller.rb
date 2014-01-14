9150a5f62b55adc223285023bdfa5215bfb4dfca
Annie Tang <annietang92@gmail.com>
2014-01-14 12:40:04 -0500

    fixed sign up to sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
+      #flash[:success] = "Welcome to the Sample App!"
+      #NEED TO CHANGE
       redirect_to @user
     else
       render 'new'
