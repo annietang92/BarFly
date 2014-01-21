9bcdfff2943c1aa8030273b1247186e510a38576
Annie Tang <annietang92@gmail.com>
2014-01-21 15:41:48 -0500

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,15 @@ ActiveRecord::Schema.define(version: 20140121203759) do
     t.datetime "updated_at"
   end
 
+  create_table "locations", force: true do |t|
+    t.string   "city"
+    t.string   "state"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
+  end
+
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
