7519567fc2fdc61d7eab16e00539f09026d141ad
Annie Tang <annietang92@gmail.com>
2014-01-21 17:31:14 -0500

    all text

--- db/migrate/20140117212708_add_attributes_to_beers.rb
+++ db/migrate/20140117212708_add_attributes_to_beers.rb
@@ -1,16 +1,16 @@
 class AddAttributesToBeers < ActiveRecord::Migration
   def change
-  	add_column :beers, :name, :string
-  	add_column :beers, :brewed_by, :string
+  	add_column :beers, :name, :text
+  	add_column :beers, :brewed_by, :text
   	add_column :beers, :img_icon, :text
   	add_column :beers, :img_med, :text
   	add_column :beers, :img_lg, :text
-  	add_column :beers, :style, :string
-  	add_column :beers, :style_type, :string
+  	add_column :beers, :style, :text
+  	add_column :beers, :style_type, :text
   	add_column :beers, :description, :text
-  	add_column :beers, :abv, :string
-  	add_column :beers, :ibu, :string
-  	add_column :beers, :is_organic, :string
+  	add_column :beers, :abv, :text
+  	add_column :beers, :ibu, :text
+  	add_column :beers, :is_organic, :text
   	add_column :beers, :likes, :integer
   	add_column :beers, :total, :integer
   	add_column :beers, :unique, :integer
