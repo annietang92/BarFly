297b386815c64082b334f29d427f8501bce38154
Annie Tang <annietang92@gmail.com>
2014-01-25 19:26:20 -0500

    changed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -65,9 +65,9 @@ open(File.open(File.join(Rails.root, '/SeedData/Cocktails.txt'))) do |file|
 		cocktail.served = line_array[4]
 		cocktail.glass = line_array[5]
 		cocktail.ingredients = line_array[6]
-		cocktail.instructions = line_array[7]
+		# cocktail.instructions = line_array[7]
 		cocktail.description = line_array[8]
-		# cocktail.image = line_array[9]
+		cocktail.image = line_array[9]
 		if !line_array[10].nil?
 			cocktail.likes = Integer(line_array[10])
 		end
