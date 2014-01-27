69aa409b1cc085ece2fb0b028b23d1288d636784
Annie Tang <annietang92@gmail.com>
2014-01-27 13:52:50 -0500

    fixed venue error

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -18,10 +18,15 @@ class BeersController < ApplicationController
 			@drink.drink_id = @beer.id
 			@drink.comment = current_user.name.to_s + " just created a new beer!"
 			if @drink.save
+				@venue.city = current_user.location
+				client = Foursquare2::Client.new(:client_id => '4IEC2TEEXYZBNXI4Z0XWKOPCXPTL54WIYGSL20IRJVOH41QT', :client_secret => 'U1W5VEBHWP4I020DJL1HQ14MTRCQAMMDKH554VTYKUG4BEGF')
+				if !current_user.location.nil? || current_user.location != ""
+					@top_venues = client.search_venues(:near => @user.location, categoryId: '4bf58dd8d48988d116941735', limit: 20).groups[0].items
+				end
 				flash.now[:success] = "New drink recorded! You can add a venue for your drink, or skip this step."
 				render 'venues/new'
 			else
-				flash.now[:error] = "Something went wrong, please try again."
+				flash[:error] = "Something went wrong, please try again."
 				render 'drinks/new'
 			end
 		else
