7c36760cbf6e28758608b7d22c5a9235f869eb0a
Annie Tang <annietang92@gmail.com>
2014-01-28 22:09:10 -0500

    dkhfsdklh

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,11 +44,6 @@ class UsersController < ApplicationController
       no_flash = true
     end
     @user = User.find(params[:id])
-    if !LocationCache.is_valid_location(@user.location)
-      flash[:error] = "Please update with a valid location"
-      redirect_to edit_user_path(current_user)
-      return
-    end
     @locations = LocationCache.last.text
     if @user.update_attributes(user_params)
       if !no_flash
