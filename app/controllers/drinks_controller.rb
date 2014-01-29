c8f808f34594776e0c2554e13750fc29a3f10a2c
Annie Tang <annietang92@gmail.com>
2014-01-28 21:42:03 -0500

    guh

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -13,11 +13,6 @@ class DrinksController < ApplicationController
 	end
 
 	def create
-		if current_user.location.nil?
-			flash[:error] = "Please choose and save your current city."
-			redirect_to edit_user_path(current_user)
-			return
-		end
 		if !LocationCache.is_valid_location(current_user.location)
 			flash[:error] = "Please update with a valid location"
 			redirect_to edit_user_path(current_user)
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,8 +3,8 @@ class StaticPagesController < ApplicationController
 
 	def index
 		if signed_in?
-			# @top_venues = Venue.top_venues_around(current_user).take(10)
-			# @top_drinks = Drink.where(location: current_user.location).take(10)
+			@top_venues = Venue.top_venues_around(current_user).take(10)
+			@top_drinks = Drink.where(location: current_user.location).take(10)
 			@dashboard = true
 			all_drinks = Cocktail.all_cocktails + Beer.all_beers
 			@all_drinks = all_drinks.sort
