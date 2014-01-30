cab155bde09d0bab97bcfc9134232203ba2ff2d4
Annie Tang <annietang92@gmail.com>
2014-01-29 21:07:39 -0500

    kdhfld

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,12 @@ class StaticPagesController < ApplicationController
 
 	def index
 		if signed_in?
-			if !LocationCache.is_valid_location(current_user.location) || current_user.location.nil?
+			if current_user.location.nil?
+			  flash[:error] = "Please update with a valid location"
+		      redirect_to edit_user_path(current_user)
+		      return
+		  end
+			if !LocationCache.is_valid_location(current_user.location)
 		      flash[:error] = "Please update with a valid location"
 		      redirect_to edit_user_path(current_user)
 		      return
